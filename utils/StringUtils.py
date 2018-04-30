99e44050d070977a0b0f1b9209531efb71b2a692
pcae18 <pcae18@hotmail.com>
2018-04-30 14:47:02 -0500

    General/ Creación/ StringUtils Creación de helper para el manejo de cadenas

---
+++ utils/StringUtils.py
@@ -0,0 +1,30 @@
+import hashlib
+import datetime
+import time
+
+
+class StringUtils():
+	"""
+
+			Clase de ayuda encargada del manejo
+		de cadenas en el sistema.
+
+		@author Paulo_Angeles.
+
+	"""
+	@staticmethod
+	def randomHash(self):
+		"""
+			Sprint: 1
+
+				Método encargado de generar random
+			hash usando el tiempo como llave. 
+
+				Este método hace uso del algoritmo de
+			encriptamiento sha256.
+
+			@author Paulo_Angeles.
+		"""
+		milis = int(round(time.time() * 1000))
+		hash64 = hashlib.sha256(str(milis).encode('utf-8')).hexdigest()
+		return hash64
